42c94a44e26a0e5362b29e5679a27c4e86747fcb
2huk <anton31039410@gmail.com>
2017-01-20 00:48:12 +0300

    2.0

--- src/ru/levelup/java1/lesson2/homework/QuadraticEquation.java
+++ src/ru/levelup/java1/lesson2/homework/QuadraticEquation.java
@@ -1,6 +1,7 @@
 package ru.levelup.java1.lesson2.homework;
 
 import java.io.*;
+import java.util.*;
 
 /**
  * Created by anton on 17.01.2017.
@@ -15,18 +16,30 @@ public class QuadraticEquation {
         try {
             System.out.print("a = ");
             a = Double.parseDouble(reader.readLine());//вот здесь например, у try catch своя область видимости, у ЛЮБЫХ {}область видимости ограничена, или есть исклюения?
+        } catch (NumberFormatException e) {
+            System.out.println("Некорректный ввод");
+            main(null);
+            return;
         } catch (IOException e) {
             e.printStackTrace();
         }
         try {
             System.out.print("b = ");
             b = Double.parseDouble(reader.readLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Некорректный ввод");
+            main(null);
+            return;
         } catch (IOException e) {
             e.printStackTrace();
         }
         try {
             System.out.print("c = ");
             c = Double.parseDouble(reader.readLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Некорректный ввод");
+            main(null);
+            return;
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,18 +51,20 @@ public class QuadraticEquation {
             double x = -c / b;
             System.out.println("Корень уравнения:" + x);
         } else if (d > 0) {
-            double x = (-b + Math.sqrt(d)) / 2 * a;
-            double y = (-b - Math.sqrt(d)) / 2 * a;
+            double x = (-b + Math.sqrt(d)) / (2 * a);
+            double y = (-b - Math.sqrt(d)) / (2 * a);
             System.out.println("Корни уравнения:" + x + ", " + y);
         } else if (d == 0) {
-            double x = -b / 2 * a;
+            double x = -b / (2 * a);
             //double y = x;
             System.out.println("Корни уравнения:" + x + ", " + x);
         } else if (d < 0) {
             double real = -b / (2 * a);
-            double imaginary = Math.sqrt(Math.abs(d)) / 2 * a;
+            double imaginary = Math.sqrt(Math.abs(d)) / (2 * a);
             System.out.println("Корни уравнения:" + real + " + " + "i * " + imaginary + ", " + real + " - " + "i * " + imaginary);
         }
+
+
     }
 
 }
