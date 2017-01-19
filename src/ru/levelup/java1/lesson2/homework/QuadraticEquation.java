028d5ad7205ea21bc8b655ad075fa4749efd2be1
2huk <anton31039410@gmail.com>
2017-01-19 23:43:24 +0300

    111

--- src/ru/levelup/java1/lesson2/homework/QuadraticEquation.java
+++ src/ru/levelup/java1/lesson2/homework/QuadraticEquation.java
@@ -1,5 +1,7 @@
 package ru.levelup.java1.lesson2.homework;
+
 import java.io.*;
+
 /**
  * Created by anton on 17.01.2017.
  */
@@ -30,7 +32,12 @@ public class QuadraticEquation {
         }
 
         double d = Math.pow(b, 2) - 4 * a * c;
-        if (d > 0) {
+        if ((a == 0.0) && (b == 0.0)) {
+            System.out.println("Некорректный ввод");
+        } else if (a == 0.0) {
+            double x = -c / b;
+            System.out.println("Корень уравнения:" + x);
+        } else if (d > 0) {
             double x = (-b + Math.sqrt(d)) / 2 * a;
             double y = (-b - Math.sqrt(d)) / 2 * a;
             System.out.println("Корни уравнения:" + x + ", " + y);
@@ -41,7 +48,7 @@ public class QuadraticEquation {
         } else if (d < 0) {
             double real = -b / (2 * a);
             double imaginary = Math.sqrt(Math.abs(d)) / 2 * a;
-            System.out.println("Корни уравнения:" + real + " + "+"i * "+imaginary+ ", " + real + " - "+"i * "+ imaginary);
+            System.out.println("Корни уравнения:" + real + " + " + "i * " + imaginary + ", " + real + " - " + "i * " + imaginary);
         }
     }
 
