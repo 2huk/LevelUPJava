9370a7c7ff8a133786ad58e78f66e24b6943e384
2huk <anton31039410@gmail.com>
2017-02-17 16:03:06 +0300

    homework

--- src/ru/levelup/java1/lesson8/uiclasses/ImageInputWidget.java
+++ src/ru/levelup/java1/lesson8/uiclasses/ImageInputWidget.java
@@ -4,4 +4,6 @@ package ru.levelup.java1.lesson8.uiclasses;
  * Created by anton on 12.02.2017.
  */
 public class ImageInputWidget extends Container {
+    TextField urlField;
+    Button button;
 }
